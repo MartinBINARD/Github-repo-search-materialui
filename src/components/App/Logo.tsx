81f74ad4c53604dd4991c1a185e284b304d9e3fc
MartinBINARD <martin.binard.dev@gmail.com>
2023-08-23 17:29:53 +0200

    Ajout BackToTop MaterialUI + logo

---
+++ src/components/App/Logo.tsx
@@ -0,0 +1,11 @@
+import logo from '../../assets/images/logo-github.png';
+
+function Logo() {
+  return (
+    <div className="logo">
+      <img className="logo-image" src={logo} alt="logo github" />
+    </div>
+  );
+}
+
+export default Logo;
